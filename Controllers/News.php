f8090eff67a249de9bb4838d730e372022459904
MisterAnmar <59205168+MisterAnmar@users.noreply.github.com>
2022-04-30 11:48:23 +0300

    Added Cons, and fixed model name and name spacing

--- Controllers/News.php
+++ Controllers/News.php
@@ -1,12 +1,21 @@
 <?php namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
+use App\Models\NewsModel;
 
 class News extends ResourceController
 {
-    protected $modelName = 'App\Models\NewsModel';
+    protected $model;
     // There used to be a bug in version 4.0.2 now fixed in v4.0.3. No issues in json return
     protected $format    = 'json';
+	
+    // Prefered way
+    public function __construct()
+    {
+        $this->model  = model('NewsModel');
+    }
+	
+	
     // Handles GET Request (news)
     public function index()
     {
